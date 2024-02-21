26dd0ac34663c705839ff025b8b0a2f4ca4b3fb8
theatha <theathax@protonmail.com>
2024-02-21 16:21:25 +0300

    blog

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708520870',resources: [
+    cacheName: 'chirpy-1708521670',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
